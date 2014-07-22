1c5c2013bb83c74b897bc84dbf8ca119277a645b
David Mugnai <dvd@develer.com>
2014-07-22 16:14:10 +0200

    TaskClass that supports the new Celery Api

--- djcelery_transactions/__init__.py
+++ djcelery_transactions/__init__.py
@@ -16,6 +16,45 @@ def _get_task_queue():
     """Returns the calling thread's task queue."""
     return _thread_data.__dict__.setdefault("task_queue", [])
 
+import celery
+class PostTransactionTask31(celery.current_app.Task):
+    """A task whose execution is delayed until after the current transaction.
+
+    The task's fate depends on the outcome of the current transaction. If it's
+    committed or no changes are made in the transaction block, the task is sent
+    as normal. If it's rolled back, the task is discarded.
+
+    If transactions aren't being managed when ``apply_asyc()`` is called (if
+    you're in the Django shell, for example) or the ``after_transaction``
+    keyword argument is ``False``, the task will be sent immediately.
+
+    A replacement decorator is provided:
+
+    .. code-block:: python
+
+        from djcelery_transactions import task
+
+        @task
+        def example(pk):
+            print "Hooray, the transaction has been committed!"
+    """
+
+    abstract = True
+
+    def original_apply_async(self, *args, **kwargs):
+        """Shortcut method to reach real implementation
+        of celery.Task.apply_sync
+        """
+        return super(PostTransactionTask31, self).apply_async(*args, **kwargs)
+
+    def apply_async(self, *args, **kwargs):
+        # Delay the task unless the client requested otherwise or transactions
+        # aren't being managed (i.e. the signal handlers won't send the task).
+        connection = get_connection()
+        if connection.in_atomic_block and not getattr(current_app.conf, 'CELERY_ALWAYS_EAGER', False):
+            _get_task_queue().append((self, args, kwargs))
+        else:
+            return self.original_apply_async(*args, **kwargs)
 
 class PostTransactionTask(Task):
     """A task whose execution is delayed until after the current transaction.
